271535db41d2367008e49ce0becff41a033f62f7
Mahmoud Reda <m.elshentenawy1996@gmail.com>
2022-10-14 22:46:23 +0200

    some edit

--- js/index.js
+++ js/index.js
@@ -1,75 +1,14 @@
-let menuBtn = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .flex .navbar');
+let menuBtn = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .flex .navbar");
+let nav = document.querySelectorAll(".header .flex .navbar a");
 
-menuBtn.onclick = () =>{
-  menuBtn.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
-
-// var swiper = new Swiper(".courses-slider", {
-//   spaceBetween: 20,
-//   grabCursor : true,
-//   loop : true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//   clickable:true,
-
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 1,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//     },
-//     1024: {
-//       slidesPerView: 3,
-
-//     },
-//   },
-// });
-
-// var swiper = new Swiper(".teacher-slider", {
-//   spaceBetween: 20,
-//   grabCursor : true,
-//   loop : true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//   clickable:true,
-
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 1,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//     },
-//     1024: {
-//       slidesPerView: 3,
-
-//     },
-//   },
-// });
-// var swiper = new Swiper(".reviews-slider", {
-//   spaceBetween: 20,
-//   grabCursor : true,
-//   loop : true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//   clickable:true,
-
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 1,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//     },
-//     1024: {
-//       slidesPerView: 3,
-
-//     },
-//   },
-// });
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+nav.forEach((item) => {
+  item.onclick = () => {
+    navbar.classList.toggle("active");
+    menuBtn.classList.toggle("fa-times");
+  };
+});
